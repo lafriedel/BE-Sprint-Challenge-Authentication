c884b0fbe28068cc00538ea2a76477775eecc976
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-03-01 11:45:01 -0500

    Move generateToken to separate file

--- config/routes.js
+++ config/routes.js
@@ -1,14 +1,14 @@
-const axios = require('axios');
+const axios = require("axios");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 const db = require("../database/dbConfig");
 
-const { authenticate, jwtKey } = require('../auth/authenticate');
+const { authenticate } = require("../auth/authenticate");
+const tokenService = require("../auth/generate-token");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
 function register(req, res) {
@@ -17,61 +17,60 @@ function register(req, res) {
   const hash = bcrypt.hashSync(user.password, 10);
   user.password = hash;
 
-  db("users").insert(user)
-  .then(userId => {
-    const [id] = userId;
-    db("users").where("id", id).then(user => {
-      res.status(201).json(user);
+  db("users")
+    .insert(user)
+    .then(userId => {
+      const [id] = userId;
+      db("users")
+        .where("id", id)
+        .then(user => {
+          res.status(201).json(user);
+        });
     })
-  })
-  .catch(err => {
-    res.status(500).json({error: "There was an error registering the user."})
-  })
-}
-
-function generateToken(user) {
-  const payload = {
-    subject: user.id,
-    username: user.username,
-  };
-  const options = {
-    expiresIn: '1d'
-  };
-
-  return jwt.sign(payload, jwtKey, options)
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "There was an error registering the user." });
+    });
 }
 
 function login(req, res) {
   let { username, password } = req.body;
 
-  db("users").where("username", username).first()
+  db("users")
+    .where("username", username)
+    .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        const token = generateToken(user);
+        const token = tokenService.generateToken(user);
         res.status(200).json({
           message: `Welcome, ${user.username}`,
           token
-        })
+        });
       } else {
-        res.status(401).json({error: "invalid credentials, please try again."})
+        res
+          .status(401)
+          .json({ error: "Invalid credentials, please try again." });
       }
     })
     .catch(err => {
-      res.status(500).json({message: "There was an error logging in the user."})
-    })
+      res
+        .status(500)
+        .json({ message: "There was an error logging in the user." });
+    });
 }
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
