c594550849bcba1cee43b3b01c9c64d90b4d7444
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-03-01 11:14:12 -0500

    Successfully implement register function

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const bcrypt = require("bcryptjs");
+const db = require("../database/dbConfig");
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -9,7 +11,21 @@ module.exports = server => {
 };
 
 function register(req, res) {
-  // implement user registration
+  let user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  db("users").insert(user)
+  .then(userId => {
+    const [id] = userId;
+    db("users").where("id", id).then(user => {
+      res.status(201).json(user);
+    })
+  })
+  .catch(err => {
+    res.status(500).json({error: "There was an error registering the user."})
+  })
 }
 
 function login(req, res) {
